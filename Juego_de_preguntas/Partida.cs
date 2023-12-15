f94c37901ab76835f06694d4486efa7d6d696f55
GABRIEL PORRAS PÉREZ <gabporper@alu.edu.gva.es>
2023-12-15 14:41:35 +0100

    Añadidas las clases del Model

---
+++ Juego_de_preguntas/Partida.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_de_preguntas
+{
+    internal class Partida
+    {
+        private List<Pregunta> listaPreguntas;
+        public List<Pregunta> ListaPreguntas
+        {
+            get { return listaPreguntas; }
+            set { SetProperty(ref listaPreguntas, value); }
+        }
+
+        private string dificultad;
+        public string Difucultad
+        {
+            get { return dificultad; } 
+            set { SetProperty(ref dificultad, value); }
+        }
+
+        public Partida(List<Pregunta> listaPreguntas, string dificultad)
+        {
+            this.listaPreguntas = listaPreguntas;
+            this.dificultad = dificultad;
+        }
+
+        private void SetProperty(ref string dificultad, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SetProperty(ref List<Pregunta> listaPreguntas, List<Pregunta> value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Juego_de_preguntas/Pregunta.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_de_preguntas
+{
+
+    internal class Pregunta : INotifyPropertyChanged
+    {
+        private string textoPregunta;
+        public string TextoPregunta
+        {
+            get => textoPregunta;
+            set
+            {
+                textoPregunta = value;
+                NotifyPropertyChanged("Pregunta");
+            }
+        }
+
+        private string respuesta;
+        public string Respuesta
+        {
+            get => respuesta;
+            set
+            {
+                respuesta = value;
+                NotifyPropertyChanged("Respuesta");
+            }
+        }
+
+        private string imagen;
+        public string Imagen
+        {
+            get => imagen;
+            set
+            {
+                imagen = value;
+                NotifyPropertyChanged("Imagen");
+            }
+        }
+
+        private string dificultad;
+        public string Dificultad
+        {
+            get => dificultad;
+            set
+            {
+                dificultad = value;
+                NotifyPropertyChanged("Dificultad");
+            }
+        }
+
+        private string categoria;
+        public string Categoria
+        {
+            get => categoria;
+            set
+            {
+                categoria = value;
+                NotifyPropertyChanged("Categoria");
+            }
+        }
+
+        public Pregunta(string textoPregunta, string respuesta, string imagen, string dificultad, string categoria)
+        {
+            this.textoPregunta = textoPregunta;
+            this.respuesta = respuesta;
+            this.imagen = imagen;
+            this.dificultad = dificultad;
+            this.categoria = categoria;
+        }
+
+        public Pregunta() { }
+
+        private void NotifyPropertyChanged(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
